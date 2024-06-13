19751ae8d282f075ca18fc7933ed82a07fa029c1
natalita-test <natalita.seica@gmail.com>
2024-06-13 22:11:00 +0300

    fixed migrations

--- DAL/ApplicationDbContext.cs
+++ DAL/ApplicationDbContext.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Data.Common;
-using System.Data.Entity;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using DAL.Entities;
 
 
 namespace DataAccessLayer
 {
     public class ApplicationDbContext : DbContext
-	{
+    {
 
 
         //public ApplicationDbContext(string connectionString) : base(connectionString)
@@ -15,49 +14,51 @@ namespace DataAccessLayer
         //}
 
         public ApplicationDbContext() : base("name=MyDbContext")
-		{
+        {
 
-		}
+        }
 
         public DbSet<CityEntity> Cities => Set<CityEntity>();
-		public DbSet<ConnectionTypeEntity> ConnectionTypes => Set<ConnectionTypeEntity>();
-		public DbSet<IssueEntity> Issues => Set<IssueEntity>();
-		public DbSet<IssuesTypeEntity> IssuesType => Set<IssuesTypeEntity>();
-		public DbSet<LogEntity> Logs => Set<LogEntity>();
-		public DbSet<PosEntity> Pos => Set<PosEntity>();
-		public DbSet<StatusEntity> Statuses => Set<StatusEntity>();
-		public DbSet<UserEntity> Users => Set<UserEntity>();
-		public DbSet<UserTypeEntity> UserTypes => Set<UserTypeEntity>();
+        public DbSet<ConnectionTypeEntity> ConnectionTypes => Set<ConnectionTypeEntity>();
+        public DbSet<IssueEntity> Issues => Set<IssueEntity>();
+        public DbSet<IssuesTypeEntity> IssuesType => Set<IssuesTypeEntity>();
+        public DbSet<LogEntity> Logs => Set<LogEntity>();
+        public DbSet<PosEntity> Pos => Set<PosEntity>();
+        public DbSet<StatusEntity> Statuses => Set<StatusEntity>();
+        public DbSet<UserEntity> Users => Set<UserEntity>();
+        public DbSet<UserTypeEntity> UserTypes => Set<UserTypeEntity>();
 
 
 
-		protected override void OnModelCreating(DbModelBuilder modelBuilder)
-		{
-			//User
-			modelBuilder.Entity<UserEntity>().HasRequired(u => u.UserType).WithMany(c => c.Users).HasForeignKey(u => u.UserTypeId);
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            //User
+            modelBuilder.Entity<UserEntity>().HasRequired(u => u.UserType).WithMany(c => c.Users).HasForeignKey(u => u.UserTypeId);
 
 
-			//Pos
-			modelBuilder.Entity<PosEntity>().HasRequired(u => u.Cities).WithMany(c => c.Pos).HasForeignKey(u => u.City_Id).WillCascadeOnDelete(false);
-			modelBuilder.Entity<PosEntity>().HasRequired(u => u.ConnectionType).WithMany(c => c.Pos).HasForeignKey(u => u.ConnType_Id);
+            //Pos
+            modelBuilder.Entity<PosEntity>().HasRequired(u => u.Cities).WithMany(c => c.Pos).HasForeignKey(u => u.City_Id);
+            modelBuilder.Entity<PosEntity>().HasRequired(u => u.ConnectionType).WithMany(c => c.Pos).HasForeignKey(u => u.ConnType_Id);
 
-			//Logs
-			modelBuilder.Entity<LogEntity>().HasRequired(u => u.User).WithMany(c => c.Logs).HasForeignKey(u => u.IdUser);
-			modelBuilder.Entity<LogEntity>().HasRequired(u => u.Issues).WithMany(c => c.Logs).HasForeignKey(u=>u.IdIssue);
-
-
-			//Issues
-			modelBuilder.Entity<IssueEntity>().HasRequired(u => u.Pos).WithMany(c => c.Issues).HasForeignKey(u => u.IdPos);
-			modelBuilder.Entity<IssueEntity>().HasRequired(u => u.User).WithMany(c => c.Issues).HasForeignKey(u => u.IdUserCreated);
-			modelBuilder.Entity<IssueEntity>().HasRequired(u => u.UserType).WithMany(c => c.Issues).HasForeignKey(u => u.IdUserType);
-			modelBuilder.Entity<IssueEntity>().HasRequired(u => u.IssuesType).WithMany(c => c.Issues).HasForeignKey(u => u.IdType);
-			modelBuilder.Entity<IssueEntity>().HasRequired(u => u.Status).WithMany(c => c.Issues).HasForeignKey(u => u.IdStatus);
-			
-			
-           
-		}
+            //Logs
+            modelBuilder.Entity<LogEntity>().HasRequired(u => u.User).WithMany(c => c.Logs).HasForeignKey(u => u.IdUser);
+            modelBuilder.Entity<LogEntity>().HasRequired(u => u.Issues).WithMany(c => c.Logs).HasForeignKey(u => u.IdIssue);
 
 
-	}
+            //Issues
+            modelBuilder.Entity<IssueEntity>().HasRequired(u => u.Pos).WithMany(c => c.Issues).HasForeignKey(u => u.IdPos);
+            modelBuilder.Entity<IssueEntity>().HasRequired(u => u.User).WithMany(c => c.Issues).HasForeignKey(u => u.IdUserCreated);
+            modelBuilder.Entity<IssueEntity>().HasRequired(u => u.UserType).WithMany(c => c.Issues).HasForeignKey(u => u.IdUserType);
+            modelBuilder.Entity<IssueEntity>().HasRequired(u => u.IssuesType).WithMany(c => c.Issues).HasForeignKey(u => u.IdType);
+            modelBuilder.Entity<IssueEntity>().HasRequired(u => u.Status).WithMany(c => c.Issues).HasForeignKey(u => u.IdStatus);
+
+
+
+        }
+
+
+    }
 
 }
--- WebApp/Fluent Validation/LoginValidation/LoginModelValidator.cs
+++ WebApp/Fluent Validation/LoginValidation/LoginModelValidator.cs
@@ -1,31 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using BLL.Repositories;
-using BLL.Repositories.User;
-using FluentValidation;
+﻿using FluentValidation;
 using WebApp.Models;
 
 namespace WebApp.Fluent_Validation.LoginValidation
 {
-	public class LoginModelValidator:AbstractValidator<LoginModel>
+    public class LoginModelValidator:AbstractValidator<LoginModel>
 	{
-		private readonly IUserRepository _userRepository;
-
-		public LoginModelValidator(IUserRepository userRepository)
+		public LoginModelValidator()
 		{
-			_userRepository = userRepository;
 
-			RuleFor(x => x.Login).NotNull();
-			RuleFor(x => x.Password).NotNull();
-
-			RuleFor(x => x.Login).NotEmpty().Must(login=>_userRepository.ExistLogin(login)).WithMessage("Don't exist user with this login");
-		}
-		public LoginModelValidator() { }
-		
+			RuleFor(x => x.Login).NotNull().WithMessage("test login error");
+			RuleFor(x => x.Password).NotNull().WithMessage("test password error");
+		}		
 	}
-
 
 }
